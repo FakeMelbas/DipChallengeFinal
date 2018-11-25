18391a8c02d1f84426243a8be6a88690986e35f1
Dylan Peterson <101408728@student.swin.edu.au>
2018-11-26 08:25:15 +1100

    Initial

---
+++ DipChallengeFinal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DipChallengeFinal.Startup))]
+namespace DipChallengeFinal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
